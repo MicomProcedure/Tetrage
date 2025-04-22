866e6ac1277622305f7d54d24a8e8730b480e684
mmm_yuto <manri.yuto.t8@dc.tohoku.ac.jp>
2025-04-22 11:07:01 +0900

    isuue#11と#10の実装

---
+++ Tetrage/Assets/Main/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Tetrage.Core
+{
+    /// <summary>
+    /// ゲーム内のプレイヤーを表します。
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// プレイヤーの一意な識別子。
+        /// </summary>
+        public int PlayerID;
+
+        /// <summary>
+        /// 現在選択されているターゲットカード。
+        /// </summary>
+        public Card target;
+
+        /// <summary>
+        /// プレイヤーが所持している手札の一覧。
+        /// </summary>
+        public List<Card> hands;
+
+        /// <summary>
+        /// 一時的に保持しているカードの一覧。
+        /// </summary>
+        public List<Card> tmp;
+
+        /// <summary>
+        /// 指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="action">実行するゲームアクション。</param>
+        public void PerformAction(GameAction action)
+        {
+            // アクションの処理内容をここに記述
+        }
+    }
+}
