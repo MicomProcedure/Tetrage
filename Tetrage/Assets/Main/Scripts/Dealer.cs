6cb09e81d35ea0d6f30798a8967ebb8cf2eda752
Mira_Ib <174165394+Mira-Ib@users.noreply.github.com>
2025-04-23 01:37:40 +0900

    Dealer.csを編集(エラー有) #4

--- Tetrage/Assets/Main/Scripts/Dealer.cs
+++ Tetrage/Assets/Main/Scripts/Dealer.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ゲームのディーラークラス。カードの配布、ターン管理、勝敗判定を行う。
+/// </summary>
 public class Dealer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 現在のステージ情報。
+    /// </summary>
+    private Stage stage;
+
+    /// <summary>
+    /// ゲームに参加しているプレイヤーのリスト。
+    /// </summary>
+    private List<Player> players;
+
+    /// <summary>
+    /// 現在のプレイヤー。
+    /// </summary>
+    private Player currentPlayer;
+
+    /// <summary>
+    /// 各プレイヤーにカードを配布する。
+    /// </summary>
+    public void DistributeCards()
+    {
+
+    }
+
+    /// <summary>
+    /// 最初にターンを持つプレイヤーを決定する。
+    /// </summary>
+    public void DecideFirstPlayer()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 次のプレイヤーのターンに移行する。
+    /// </summary>
+    public void NextTurn()
     {
-        
+
+    }
+
+    /// <summary>
+    /// 指定されたアクションタイプに基づいてテトラージュの勝敗を判定する。
+    /// </summary>
+    public void JudgeTetrage(ActionType actionType)
+    {
+
     }
 }
