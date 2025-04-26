7358aaaa39a5a9aa870e9a3719780fceeafd5894
mmm_yuto <manri.yuto.t8@dc.tohoku.ac.jp>
2025-04-26 10:44:49 +0900

    自分のカードが一致していたらチェックボタンを出す． 簡易的なステージを作成

--- Tetrage/Assets/Main/Scripts/Tetrage/Actions/Check.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Actions/Check.cs
@@ -6,11 +6,17 @@ using Tetrage.Models;
 public class Check : MonoBehaviour
 {
     private Player player1;
+    public GameObject cardPrefab;
+    public GameObject CheckButton;
+    
 
+    //チェックボタンが押されたかどうか
+    public bool CheckButtonPushed = false;
+
+    [Header("デバック用")]
     public bool DebugMode = true;
-
-    public GameObject cardPrefab; // Inspector で Card プレハブを設定
-    public Transform[] cardSpawnPoints; // 配置場所を設定
+    public Transform[] MyCardSpawn; //自分のカード配置場所を設定
+    public Transform[] OtherCardSpawn; //相手のカード配置場所
 
     void Start()
     {
@@ -23,7 +29,20 @@ public class Check : MonoBehaviour
             AddCardToPlayer(Card.Suit.Spade, 5, true);
         }
 
-        CheckAllCard(player1);
+        //自分の手札が一致しているかどうか取得
+        if(CheckAllCardSame(player1)){
+            //手札が一致していたらチェックボタンを出す
+            CheckButton.SetActive(true);
+        }
+        
+    }
+
+    void Update()
+    {
+        //チェックボタンが押されたら相手のカードを選択させるフェーズに入る
+        if(CheckButtonPushed){
+
+        }
     }
 
     private void AddCardToPlayer(Card.Suit suit, int number, bool isVisible)
@@ -33,9 +52,9 @@ public class Check : MonoBehaviour
 
         // スポーン位置に配置（適宜調整）
         int index = player1.hands.Count;
-        if (index < cardSpawnPoints.Length)
+        if (index < MyCardSpawn.Length)
         {
-            cardObj.transform.position = cardSpawnPoints[index].position;
+            cardObj.transform.position = MyCardSpawn[index].position;
         }
 
         // Card スクリプトを取得して初期化
@@ -46,7 +65,7 @@ public class Check : MonoBehaviour
         player1.hands.Add(card);
     }
 
-    private bool CheckAllCard(Player player)
+    private bool CheckAllCardSame(Player player)
     {
         if (player.hands == null || player.hands.Count == 0)
         {
@@ -65,5 +84,12 @@ public class Check : MonoBehaviour
         }
 
         return true;
+    }
+
+    //チェックボタンの機能
+    public void DoCheck()
+    {
+        CheckButton.SetActive(false);
+        CheckButtonPushed = true;
     }
 }
