4a56ee04f99f18dca8249150e1d23138969938d6
555takakusaki <555crocky@gmail.com>
2025-04-28 18:39:09 +0900

    feat: OpenActionのValidateを実装

--- Tetrage/Assets/Main/Scripts/Tetrage/Actions/OpenAction.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Actions/OpenAction.cs
@@ -1,26 +1,34 @@
 using UnityEngine;
 using Tetrage.Models;
+using Tetrage.Managers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tetrage.Actions
 {
     public class OpenAction : GameAction
     {
         private Player _requester;
-        private Player _targetPlayer;
+        private IReadOnlyList<Player> _others;
         private Card _card;
 
-        public OpenAction(Player requester, Card card, Player targetPlayer)
+        public OpenAction(Player requester)
         {
             _requester = requester;
-            _card = card;
-            _targetPlayer = targetPlayer;
+            // 他プレイヤーの参照を書き込み
+            _others = Dealer.Instance.Players
+                                       .Where(p => !ReferenceEquals(p, requester)).ToList();
         }
 
         public override bool Validate(Player player)
         {
             // Openの条件判定
 
-            return true;
+            /* もし他プレイヤーの手札に一枚でも裏のカードがあれば実行可能、つまりtrueを返す
+             * _othersはrequester以外のプレイヤーの列挙
+             * CardPileであるHandsは直接列挙可能（IEnumerable<Card>を実装している）なので.Anyが使える
+             */
+            return _others.Any(p => p.Hands.Any(c => !c.isVisible));
         }
 
         public override void Execute(Player player)
