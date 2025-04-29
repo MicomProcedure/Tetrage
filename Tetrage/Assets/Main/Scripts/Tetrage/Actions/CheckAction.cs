b3fb4778751d1bf123047f5df27483689fcab44d
555takakusaki <555crocky@gmail.com>
2025-04-29 13:39:26 +0900

    bugfix: CheckActionに最低限必要なメンバの定義

--- Tetrage/Assets/Main/Scripts/Tetrage/Actions/CheckAction.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Actions/CheckAction.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using Tetrage.Models;
+using Tetrage.Managers;
+using Tetrage.Core.Contracts;
 
 namespace Tetrage.Actions
 {
     public class CheckAction : GameAction
     {
-        private Player _requester;
-        private Player _targetPlayer;
+        // requesterは基底クラスで定義
+        private Player _targetPlayer; 
+        private IPlayerProvider _provider; // 基本はDealer、テスト用にそれ以外
 
-        public CheckAction(Player requester) : base(requester)
+
+        public CheckAction(Player requester, IPlayerProvider provider = null) : base(requester)
         {
-            _requester = requester;
- 
+            _provider = provider ?? Dealer.Instance; // providerを受け取るが、デフォルトではDealerの単一なインスタンスとなる
+
         }
 
         public override bool Validate()
