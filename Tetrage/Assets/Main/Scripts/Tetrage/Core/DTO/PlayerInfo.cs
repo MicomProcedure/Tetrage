fd8c8cf217b42e8e9941074754133d73a93964f1
555takakusaki <555crocky@gmail.com>
2025-05-28 19:17:00 +0900

    feat: Player作成に利用する構造体であるPlayerInfoの作成
    一旦、名前とPlayerType（RemotePlayer, LocalPlayer, Bot）だけ定義してあります

---
+++ Tetrage/Assets/Main/Scripts/Tetrage/Core/DTO/PlayerInfo.cs
@@ -0,0 +1,11 @@
+using Tetrage.Core.Enums;
+
+namespace Tetrage.Core.DTO
+{
+    public struct PlayerInfo
+    {
+        public string UserId;
+        public PlayerType PlayerType;
+        
+    }
+}
