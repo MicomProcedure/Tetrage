023374d61cc79f901e828afe1605f037e019faf7
555Crocky <104741018+555Crocky@users.noreply.github.com>
2025-07-15 17:04:18 +0900

    feat: CardクラスのToStringメソッドをオーバーライドし、デバッグで使いやすいように

--- Tetrage/Assets/Main/Scripts/Tetrage/Models/Card.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Models/Card.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 using System;
 using Tetrage.Core.Enums;
-using Tetrage.Core.Contracts;
 
 namespace Tetrage.Models
 {
@@ -103,6 +101,11 @@ namespace Tetrage.Models
         public void Unhighlight()
         {
             IsHighlighted = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Suit} {Number}";
         }
     }
 }
