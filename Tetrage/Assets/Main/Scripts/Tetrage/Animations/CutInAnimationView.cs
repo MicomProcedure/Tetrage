58b982333e0200c78cda11d1acf9c7cc4f89f3b9
Mira_Ib <174165394+Mira-Ib@users.noreply.github.com>
2025-10-19 17:10:50 +0900

    3種類のカットインを追加しました

--- Tetrage/Assets/Main/Scripts/Tetrage/Animations/CutInAnimationView.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Animations/CutInAnimationView.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +10,34 @@ public class CutInAnimationView : MonoBehaviour
 {
     [SerializeField] private GameObject cutInUI;
     [SerializeField] private Image cutInImage;
+    [SerializeField] private GameObject cutInText;
     [SerializeField] private Image cutInBackgroundMask;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip cutInSE;
 
     public async UniTask StartCutIn(CancellationToken token)
     {
         var backgroundMaskRect = cutInBackgroundMask.GetComponent<RectTransform>();
         var characterRect = cutInImage.GetComponent<RectTransform>();
+        var textRect = cutInText.GetComponent<RectTransform>();
         cutInUI.SetActive(true);
 
         // 位置を初期化
         await UniTask.WhenAll(
-            characterRect.DOAnchorPos(new Vector2(1600, 0), 0.0f).ToUniTask(cancellationToken: token),
+            characterRect.DOAnchorPos(new Vector2(1400, 0), 0.0f).ToUniTask(cancellationToken: token),
+            textRect.DOAnchorPos(new Vector2(1900, 50), 0.0f).ToUniTask(cancellationToken: token),
             backgroundMaskRect.DOAnchorPos(new Vector2(300, 375), 0.0f).ToUniTask(cancellationToken: token),
             backgroundMaskRect.DOSizeDelta(new Vector2(0, 2500), 0.0f).ToUniTask(cancellationToken: token)
         );
 
+        audioSource.PlayOneShot(cutInSE);
+
         // カットインアニメーションの再生
         await UniTask.WhenAll(
-            characterRect.DOAnchorPos(new Vector2(-400, 0), 0.5f).SetEase(Ease.OutCubic)
+            characterRect.DOAnchorPos(new Vector2(-600, 0), 0.5f).SetEase(Ease.OutCubic)
+                .ToUniTask(cancellationToken: token),
+            textRect.DOAnchorPos(new Vector2(-100, 50), 0.5f).SetEase(Ease.OutCubic)
                 .ToUniTask(cancellationToken: token),
             backgroundMaskRect.DOSizeDelta(new Vector2(2000, 2500), 0.5f).SetEase(Ease.OutCubic)
                 .ToUniTask(cancellationToken: token)
@@ -36,7 +48,9 @@ public class CutInAnimationView : MonoBehaviour
 
         // カットインの終了アニメーションの再生
         await UniTask.WhenAll(
-            characterRect.DOAnchorPos(new Vector2(-1500, 0), 0.3f).SetEase(Ease.InCubic)
+            characterRect.DOAnchorPos(new Vector2(-1700, 0), 0.3f).SetEase(Ease.InCubic)
+                .ToUniTask(cancellationToken: token),
+            textRect.DOAnchorPos(new Vector2(-1200, 50), 0.3f).SetEase(Ease.InCubic)
                 .ToUniTask(cancellationToken: token),
             backgroundMaskRect.DOAnchorPos(new Vector2(-1500, 375), 0.3f).SetEase(Ease.InCubic)
                 .OnComplete(() => { cutInUI.SetActive(false); })
