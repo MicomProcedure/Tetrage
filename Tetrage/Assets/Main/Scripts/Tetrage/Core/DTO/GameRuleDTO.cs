223b405d2c2425c233f744eda3473f80db7f9588
555Crocky <104741018+555Crocky@users.noreply.github.com>
2025-11-22 00:00:16 +0900

    refactor: GameRuleDTOの調整とPlayerInfoをrecord型へと変更
    - PlayerInfoのリストを渡すことでゲームルールとの整合性を確かめるExtentionを作成
    - PlayerInfoをstructからrecordへと変更

--- Tetrage/Assets/Main/Scripts/Tetrage/Core/DTO/GameRuleDTO.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Core/DTO/GameRuleDTO.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Tetrage.Core.Enums;
 using Tetrage.Core.Ids;
+using Tetrage.Core.Constants;
 
 namespace Tetrage.Core.DTO
 {
@@ -12,37 +13,74 @@ namespace Tetrage.Core.DTO
     public sealed record GameRuleDTO
     {
         /// <summary>参加プレイヤー数。</summary>
-        public int PlayerCount { get; init; }
+        public int PlayerCount { get; private set; }
 
         /// <summary>スートごとのカード枚数。</summary>
-        public IReadOnlyDictionary<Suit, int> CardsPerSuit { get; init; }
+        public int CardCountPerSuit { get; private set; }
 
         /// <summary>ゲーム内で使用するスートの種類数。</summary>
-        public int SuitTypeCount { get; init; }
+        public int SuitTypeCount { get; private set; }
 
-        /// <summary>ゲームに参加するプレイヤー情報一覧。</summary>
-        public IReadOnlyList<PlayerInfo> Participants { get; init; }
 
         /// <summary>
         /// コンストラクタ。
         /// </summary>
         public GameRuleDTO(
             int playerCount,
-            IReadOnlyDictionary<Suit, int> cardsPerSuit,
-            int suitTypeCount,
-            IReadOnlyList<PlayerInfo> players)
+            int cardCountPerSuit,
+            int suitTypeCount)
         {
-            if (playerCount <= 0)
+            ValidateGameRule(playerCount, cardCountPerSuit, suitTypeCount);
+
+            PlayerCount = playerCount;
+            CardCountPerSuit = cardCountPerSuit;
+            SuitTypeCount = suitTypeCount;
+        }
+        #endregion
+
+        #region Factory Methods
+
+        public GameRuleDTO Default => new GameRuleDTO(
+            playerCount: InGameConsts.DEFAULT_GAME_PLAYER_COUNT,
+            cardCountPerSuit: InGameConsts.DEFAULT_INITIAL_COUNT_PER_SUIT,
+            suitTypeCount: InGameConsts.DEFAULT_INITIAL_SUITS.Length
+        );
+
+        #endregion
+
+        #region Private Validation Methods
+
+        private void ValidateGameRule(int playerCount, int cardCountPerSuit, int suitTypeCount)
+        {
+            ValidatePlayerCount(playerCount);
+            ValidateCardCountPerSuit(cardCountPerSuit);
+            ValidateSuitTypeCount(suitTypeCount);
+        }
+
+        private void ValidatePlayerCount(int playerCount)
+        {
+            if (playerCount <= SettingConsts.MIN_PLAYER_COUNT || playerCount > SettingConsts.MAX_PLAYER_COUNT)
             {
                 throw new ArgumentOutOfRangeException(nameof(playerCount), "プレイヤー数は1以上で指定してください。");
             }
-
-            PlayerCount = playerCount;
-            CardsPerSuit = cardsPerSuit ?? throw new ArgumentNullException(nameof(cardsPerSuit));
-            SuitTypeCount = suitTypeCount;
-            Participants = players ?? throw new ArgumentNullException(nameof(players));
         }
-    }
-    #endregion
-}
 
+        private void ValidateCardCountPerSuit(int cardCountPerSuit)
+        {
+            if (cardCountPerSuit <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(cardCountPerSuit), "カード枚数は1以上で指定してください。");
+            }
+        }
+
+        private void ValidateSuitTypeCount(int suitTypeCount)
+        {
+            if (suitTypeCount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(suitTypeCount), "スートの種類数は1以上で指定してください。");
+            }
+        }
+
+        #endregion
+    }
+}
