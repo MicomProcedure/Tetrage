4de5dec11e3cbd0b45b6fc83e1a63059f50b0310
岸勇斗 <yuuto77xx@gmail.com>
2026-05-08 20:19:31 +0900

    カード移動時に音がなるようにした
    CardPilePresenterでカード移動が起こった時にInGameSEから音がなるようにした

--- Tetrage/Assets/Main/Scripts/Tetrage/Presenters/CardPilePresenter.cs
+++ Tetrage/Assets/Main/Scripts/Tetrage/Presenters/CardPilePresenter.cs
@@ -56,6 +56,7 @@ namespace Tetrage.Presenters
             // ここでは単一Viewを担当する場合、_viewがselfモデルの場合のみ処理
             if (to == _model)
             {
+                InGameSE.PlayCardMoveSE();
                 _view.AddCardView(cardView);
             }
 
